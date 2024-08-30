0c38ab2efd8158bc1cc93fc07d224c6471e593b6
Nahuel1988 <copiasuco@gmail.com>
2024-08-30 15:55:03 -0300

    main

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int num1 = 2;
+        int num2 = 5;
+        if (num1 < num2) {
+            System.out.println("num1 es menor que num2");
+        } else {
+            System.out.println("num2 es mayor o igual que num1");
+
+        }
+
+    }
+}
